513524c131c4cf021c96da0826899ecb426131d9
Rodrigo Bossini <professorbossini@gmail.com>
2019-10-16 18:39:54 -0300

    Começo de ajuste para tirar fotos. Cópia de figura lançando exceção.

--- app/src/main/java/br/com/bossini/pessoal_chat_firebase_auth_storage_firestore/NovoUsuarioActivity.java
+++ app/src/main/java/br/com/bossini/pessoal_chat_firebase_auth_storage_firestore/NovoUsuarioActivity.java
@@ -1,11 +1,17 @@
 package br.com.bossini.pessoal_chat_firebase_auth_storage_firestore;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -18,6 +24,8 @@ public class NovoUsuarioActivity extends AppCompatActivity {
     private EditText loginNovoUsuarioEditText;
     private EditText senhaNovoUsuarioEditText;
     private FirebaseAuth mAuth;
+    private static final int REQ_CODE_CAMERA = 1001;
+    private ImageView pictureImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,7 @@ public class NovoUsuarioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_novo_usuario);
         loginNovoUsuarioEditText = findViewById(R.id.loginNovoUsuarioEditText);
         senhaNovoUsuarioEditText = findViewById(R.id.senhaNovoUsuarioEditText);
+        pictureImageView = findViewById(R.id.pictureImageView);
         mAuth = FirebaseAuth.getInstance();
     }
 
@@ -36,5 +45,33 @@ public class NovoUsuarioActivity extends AppCompatActivity {
             finish();
         })
         .addOnFailureListener(error -> error.printStackTrace());
+    }
+
+    public void tirarFoto (View v){
+        Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, REQ_CODE_CAMERA);
+        }else{
+            Toast.makeText(
+                    this,
+                    getString(R.string.no_camera),
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case REQ_CODE_CAMERA:
+                if (resultCode == Activity.RESULT_OK){
+                    Bitmap foto = (Bitmap) data.getExtras().get ("data");
+                    Bitmap copy = foto.copy(foto.getConfig(), true);
+                    copy.setHeight(pictureImageView.getHeight());
+                    copy.setWidth(pictureImageView.getWidth());
+                    pictureImageView.setImageBitmap(copy);
+                }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
